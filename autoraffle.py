8da81c0938e1f79ffcaa5673427f7e074b0c3b33
gale <mgastonportillo@gmail.com>
2022-07-26 17:58:33 -0300

    v2

--- autoraffle.py
+++ autoraffle.py
@@ -13,72 +13,106 @@ options = webdriver.FirefoxOptions()
 options.add_argument('--start-maximized')
 options.add_argument('--disable-extensions')
 
-# FIREFOX DRIVER PATH SETUP
-driver_path = 'C:\\Users\\gale\\vscode_repo\\python-autoraffle'
-driver = webdriver.Firefox(driver_path, firefox_options=options)
+# FIREFOX DRIVER SETUP
+driver_path = 'C:\\folder\\of\\the\\webdriver'
+driver = webdriver.Firefox(driver_path, options=options)
+wait = WebDriverWait(driver, 5)
 
 # RAFFLE SETTINGS - MODIFY FOR EVERY NEW RAFFLE
 gb = 'gb4s3'  # "Guild Boss Tamer" raffle name
 apow = 'pow4s3'  # "Player of The Week" raffle name
 
+# USEFUL ACTIONS FUNCTIONS
+
+
+class WebDriverActions:
+    """Perform Actions for the selected WebDriver"""
+
+    def __init__(self):
+        self.times = 0
+        self.actions = ActionChains(driver)
+        self.txt_str = ''
+
+    def hit_tab(self, times):
+        """Hits TAB for you"""
+        self.actions = ActionChains(driver)
+        self.times = times
+        self.actions.send_keys(Keys.TAB * times)
+        self.actions.perform()
+
+    def hit_enter(self, times):
+        """Hits ENTER for you"""
+        self.actions = ActionChains(driver)
+        self.times = times
+        self.actions.send_keys(Keys.ENTER * times)
+        self.actions.perform()
+
+    def input_args(self, txt_str):
+        """Inputs something for you"""
+        self.actions = ActionChains(driver)
+        self.txt_str = txt_str
+        self.actions.send_keys(txt_str)
+        self.actions.perform()
+
+
+# Creating instances
+tab = WebDriverActions()
+enter = WebDriverActions()
+send = WebDriverActions()
+
 # CONNECT TO DISCORD AND LOGIN
-# Open Discord
-driver.get("https://discord.com/channels/CHANNEL")
-wait = WebDriverWait(driver, 5)
-time.sleep(3)
+# Open Discord in channel
+driver.get('https://discord.com/channels/CHANNEL_ID')
+time.sleep(2)
 
 # Submit the email or phone number
 wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,
-                                      "input.inputDefault-3FGxgL input-2g-os5 inputField-2RZxdl".replace(" ", "."))))\
-    .send_keys("DISCORD_EMAIL")
-time.sleep(3)
+                                      'input.inputDefault-3FGxgL input-2g-os5 inputField-2RZxdl'.replace(' ', '.'))))\
+    .send_keys("USER_EMAIL")
+time.sleep(2)
+
 
 # My 'hack' to access the password field
+tab.hit_tab(1)
+time.sleep(2)
 
+# Submit password and click enter button
+send.input_args('USER_PASSWORD')
 
-def hack_input_filed():
-    """My 'hack' to access input fields that can't be accessed through their <tag.classes> structure"""
-    hif_n = 1
-    hif_actions = ActionChains(driver)
-    hif_actions.send_keys(Keys.TAB * hif_n)
-    hif_actions.perform()
-    time.sleep(3)
-
-
-hack_input_filed()
-
-# Submit password
+time.sleep(2)
 wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,
-                                      "button.marginBottom8-emkd0_ button-1cRKG6 button-f2h6uQ lookFilled-yCfaCM colorBrand-I6CyqQ sizeLarge-3mScP9 fullWidth-fJIsjq grow-2sR_-F".replace(" ", "."))))\
+                                      'button.marginBottom8-emkd0_ button-1cRKG6 button-f2h6uQ lookFilled-yCfaCM colorBrand-I6CyqQ sizeLarge-3mScP9 fullWidth-fJIsjq grow-2sR_-F'.replace(' ', '.'))))\
     .click()
 time.sleep(10)
 
 # My 'hack' to get into the input field
-hack_input_filed()
+tab.hit_tab(1)
+time.sleep(1)
 
 # CREATING THE RAFFLES
 # Submit command to create raffle for 'Player of the Week'
-actions = ActionChains(driver)
-actions.send_keys(f'!raffle create {apow}')
-actions.perform()
-time.sleep(3)
+send.input_args(f'!raffle create {apow}')
+time.sleep(1)
+enter.hit_enter(1)
+time.sleep(1)
 
 # Submit command to create raffle for 'Guild Boss Tamer'
-actions.send_keys(f'!raffle create {gb}')
-actions.perform()
-time.sleep(3)
+send.input_args(f'!raffle create {gb}')
+time.sleep(1)
+enter.hit_enter(1)
+time.sleep(1)
 
 # Generate the commands to add entries
 
 
-class EroicaRaffle():
+class EroicaRaffle:
     """Read specific .txt files and modify their values to turn them into accionable bot commands"""
 
     # Defining constructor
     def __init__(self):
         self.the_file = []
         self.list_names = []
-        self.commands_apow = []
+        self.commands_pow = []
         self.commands_gb = []
 
     def read_names(self, file):
@@ -88,13 +122,13 @@ class EroicaRaffle():
             self.list_names = f.read().splitlines()
         return self.list_names
 
-    def create_apow(self):
+    def create_pow(self):
         """Creates the 'Player of The Week' Award commands"""
         for line in self.list_names:
             # Simple f-string to output the bot command; max of entries is 2 and bot sums repeated entries up
-            self.commands_apow.append(
+            self.commands_pow.append(
                 f'!raffle tickets add {apow} "{line}" 1')
-        return self.commands_apow
+        return self.commands_pow
 
     def create_gb(self):
         """Creates the 'Guild Boss Tamer' Award commands"""
@@ -118,60 +152,64 @@ class EroicaRaffle():
 
 
 # Creating instance of the class
-cmds_apow = EroicaRaffle()
+raff_pow = EroicaRaffle()
 # Calling the right methods for 'Player of the Week'
-cmds_apow.read_names('names_pow.txt')
-iter_apow = cmds_apow.create_apow()
+raff_pow.read_names('names_pow.txt')
+iter_raff_pow = raff_pow.create_pow()
 
 # Creating instance of the class
-cmds_gb = EroicaRaffle()
+raff_gb = EroicaRaffle()
 # Calling the right methods for 'Guild Boss Tamer'
-cmds_gb.read_names('names_gb.txt')
-iter_gb = cmds_gb.create_gb()
+raff_gb.read_names('names_gb.txt')
+iter_raff_gb = raff_gb.create_gb()
 
 # Concatenating both lists for a smoother command submission method
-submit_comm = iter_apow + iter_gb
+submit_comm = iter_raff_pow + iter_raff_gb
 
 # SUMBITTING THE COMMANDS
 # My 'hack' to get into the input field
-hack_input_filed()
-
+tab.hit_tab(1)
+time.sleep(2)
 
 # Saving bot commands into a file and submitting them
-temp = open('commands.txt', "w")
+temp = open('commands.txt', 'w')
 for comm in submit_comm:
     temp.write(comm)
-    n = 1
-    actions.send_keys(comm)
-    actions.perform()
+    send.input_args(comm)
     time.sleep(1)
-    actions.send_keys(Keys.ENTER * n)
-    actions.perform()
-    time.sleep(4)
+    enter.hit_enter(1)
+    time.sleep(2)
 temp.close()
-time.sleep(3)
+time.sleep(2)
 
 # PERFORM RAFFLES
 # Open raffle channel
-driver.get("https://discord.com/channels/CHANNEL")
-time.sleep(3)
+driver.get('https://discord.com/channels/CHANNEL_ID')
+time.sleep(10)
 
 # My 'hack' to get into the input field
-hack_input_filed()
+tab.hit_tab(1)
+time.sleep(2)
 
 # Submit commands
-actions.send_keys(f'!raffle tickets list {gb}')
-actions.perform()
-time.sleep(3)
-actions.send_keys(f'!raffle roll {gb}')
-actions.perform()
-time.sleep(3)
-actions.send_keys(f'!raffle tickets list {gb}')
-actions.perform()
-time.sleep(3)
-actions.send_keys(f'!raffle roll {gb}')
-actions.perform()
-time.sleep(3)
+send.input_args(f'!raffle tickets list {gb}')
+time.sleep(1)
+enter.hit_enter(1)
+time.sleep(2)
+send.input_args(f'!raffle roll {gb}')
+time.sleep(1)
+enter.hit_enter(1)
+time.sleep(2)
+send.input_args(f'!raffle tickets list {gb}')
+time.sleep(1)
+enter.hit_enter(1)
+time.sleep(2)
+send.input_args(f'!raffle roll {gb}')
+time.sleep(1)
+enter.hit_enter(1)
+time.sleep(2)
+
+driver.quit()
 
 # ------------------------------------------------------------
 # --- DIRTY CODE, DON'T LOOK! (Back-up of the first draft) ---
